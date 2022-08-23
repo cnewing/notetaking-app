d0c987e395ce8c2cdb21c5f34ad1ad037465c284
Christa <christanewing47@gmail.com>
2022-08-23 13:54:02 -0400

    populate htmlroutes.js to link to specified directories

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // R E Q U I R E  D E P E N D E N C I E S
 const express = require("express");
-const epress = require("express");
 
 // C O N N E C T  R O U T E S  T O  S E R V E R
 const apiRoutes = require("./Routes/apiroutes");
@@ -10,7 +9,7 @@ const htmlRoutes = require("./Routes/htmlroutes");
 const app = express();
 
 // P O R T
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5500;
 
 // M I D D L E W A R E  T O  P A R S E  J S O N  D A T A
 app.use(express.urlencoded({ extend: true }));
@@ -20,10 +19,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // R O U T E S
-require("./Routes/apiroutes")(app);
-require("./Routes/htmlroutes")(app);
+app.use("/api", apiroutes);
+app.use("/", htmlRoutes);
 
 // L I S T E N E R
 app.listen(PORT, () => {
-  console.log("App listening on PORT: " + PORT);
+  console.log(`App listening on PORT: ${PORT}`);
 });
