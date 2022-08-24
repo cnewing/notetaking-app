020ccda1eb5785baab3029690e0d44e9d5f2669b
Christa <christanewing47@gmail.com>
2022-08-23 23:56:26 -0400

    fix inconsistencies in code

--- Routes/htmlroutes.js
+++ Routes/htmlroutes.js
@@ -1,21 +1,18 @@
 // R E Q U I R E  D E P E N D E N C I E S
-const { Router } = require("express");
-const req = require("express/lib/request");
-const res = require("express/lib/response");
 const path = require("path");
-const routeer = require("express").Router();
+const router = require("express").Router();
 
 Router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-routeer.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // D E F A U L T  T O  H O M E P A G E
-Router.get("*", (req, res) => {
+router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-module.exports = Router;
+module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require("express");
 
 // C O N N E C T  R O U T E S  T O  S E R V E R
-const apiRoutes = require("./Routes/apiroutes");
-const htmlRoutes = require("./Routes/htmlroutes");
+const apiRoutes = require("./Routes/apiRoutes");
+const htmlRoutes = require("./Routes/htmlRoutes");
 
 // E X P R E S S  S E R V E R
 const app = express();
@@ -13,16 +13,17 @@ const PORT = process.env.PORT || 5500;
 
 // M I D D L E W A R E  T O  P A R S E  J S O N  D A T A
 app.use(express.urlencoded({ extend: true }));
+
 app.use(express.json());
 
 // F O L D E R  F O R  T H E  B R O W S E R  T O  S E E
 app.use(express.static("public"));
 
 // R O U T E S
-app.use("/api", apiroutes);
+app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 // L I S T E N E R
 app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
+  console.log(`App listening on PORT ${PORT}`);
 });
