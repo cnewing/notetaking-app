4a1e49a0ac3dd27e189ac9ccded1d3cced3572f0
Christa <christanewing47@gmail.com>
2022-08-30 14:00:14 -0400

    changed mind about previous commit

--- Develop/uuid/uuid.js
+++
@@ -1,5 +0,0 @@
-//Random ID generator for newNotes ID
-module.exports = () =>
-  Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1);
--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 // R E Q U I R E  D E P E N D E N C I E S
 const express = require("express");
-const path = require("path");
-const uuid = require("./Develop/uuid/uuid");
 
-// E X P R E S S  S E R V E R  &  P O R T
+// E X P R E S S  P O R T
 const app = express();
 const PORT = process.env.PORT || 5501;
 
@@ -16,6 +14,10 @@ app.use(express.urlencoded({ extend: true }));
 app.use(express.json());
 // F O L D E R  F O R  T H E  B R O W S E R  T O  S E E
 app.use(express.static("public"));
+
+// A P I  R O U T E S
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 // L I S T E N E R
 app.listen(PORT, () => {
